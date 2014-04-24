329a8af23ef8b398158d18c94b6a56f38b20db42
Gmajoulet <gmajoulet@gmail.com>
2014-04-24 15:39:54 +0200

    Launching the test suite in all available browsers

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,58 +1,34 @@
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../',
 
+    frameworks: ['jasmine', 'detectBrowsers'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    detectBrowsers: {
+      enabled: true,
+      usePhantomJS: true
+    },
 
-
-    // list of files / patterns to load in the browser
     files: [
         'bloader.js',
         'test/test.js'
     ],
 
+    exclude: [],
 
-    // list of files to exclude
-    exclude: [
+    reporters: ['progress'], // or `dots`
 
-    ],
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
     port: 9876,
 
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
+    // All the available browsers are launched by detectBrowsers
+    // browsers: ['Chrome', 'PhantomJS', 'Firefox', 'Safari', 'IE'],
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'PhantomJS', 'Firefox', 'Safari', 'IE'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: true
   });
 };
