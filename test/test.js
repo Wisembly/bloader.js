82d9d432644e9cf11da26070f9bc0609bc480136
Gmajoulet <gmajoulet@gmail.com>
2014-04-24 15:31:12 +0200

    Switching from mocha to karma

--- test/test.js
+++ test/test.js
@@ -1,17 +1,9 @@
 (function () {
-    // Since we're in a node environment, the window object isn't available
-    window = {};
-
-    var expect = require('expect.js'),
-        sinon = require('sinon'),
-        bloader = require('../bloader');
-
     describe('Bloader.js', function () {
         describe('Instanciation', function () {
             it('should expose the loader into the window object', function () {
-                expect(true).to.be(true);
-                expect(window.Bloader).to.be.an('object');
-                expect(window.Bloader.progress).to.be.a('function');
+                expect(typeof window.Bloader).toBe('object');
+                expect(typeof window.Bloader.progress).toBe('function');
             });
         });
     });
