b186b9527588c7ee256e444f37bfe121838caa3c
Gmajoulet <gmajoulet@gmail.com>
2014-04-24 12:21:26 +0200

    Improving the gulp build to have a minified inline script

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
-    rename = require('gulp-rename');
+    insert = require ('gulp-insert'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean');
 
 gulp.task('default', function () {
-    gulp.start('scripts', 'styles');
+    gulp.start('scripts', 'styles', 'concat', 'clean');
 });
 
 gulp.task('scripts', function () {
@@ -16,7 +18,8 @@ gulp.task('scripts', function () {
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
         .pipe(gulp.dest('build'))
-        .pipe(notify({ message: 'Scripts task complete' }));
+        .pipe(insert.wrap('<script type="text/javascript">', '</script>'))
+        .pipe(gulp.dest('build/temp'));
 });
 
 gulp.task('styles', function () {
@@ -25,5 +28,18 @@ gulp.task('styles', function () {
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(gulp.dest('build'))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(insert.wrap('<style type="text/css">', '</style>'))
+        .pipe(gulp.dest('build/temp'));
+});
+
+gulp.task('concat', ['styles', 'scripts'], function () {
+    return gulp.src(['build/temp/*'])
+        .pipe(concat('bloader.min.html'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', ['concat'], function() {
+  return gulp.src(['build/temp'], {read: false})
+    .pipe(clean())
+    .pipe(notify({ message: 'Bloader build complete' }));
 });
